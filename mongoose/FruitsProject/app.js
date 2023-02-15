8941fe0ea171a1d6af4f533612c02c50fa50dc6d
Moutasim02 <moutasim9997@gmail.com>
2023-02-15 19:56:24 +0400

    Fruits project data persistence

--- mongoose/FruitsProject/app.js
+++ mongoose/FruitsProject/app.js
@@ -1,17 +1,61 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
 const fruitsSchema = new mongoose.Schema({
-  name: String,
-  rating: Number,
+  _id: Number,
+  name: {
+    type: String,
+    required: [true, "Why no name?"]
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10
+  },
   review: String,
 });
 
 const Fruit = mongoose.model("Fruit", fruitsSchema);
 
-const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
-  review: "Great taste"
+const lemon = new Fruit({
+  _id: 5,
+  name: "Lemon",
+  rating: 9,
+  review: "Tastes like lime"
 });
-fruit.save();
+
+lemon.save()
+
+// const banana = new Fruit({
+//   _id: 2,
+//   name: "banana",
+//   rating: 7,
+//   review: "Great taste"
+// });
+
+// const orange = new Fruit({
+//   _id: 3,
+//   name: "orange",
+//   rating: 7,
+//   review: "Great taste"
+// });
+
+// Fruit.insertMany([kiwi, orange, banana], (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved");
+//   }
+// })
+
+Fruit.find((err, fruits) => {
+  if(err) {
+    console.log(err);
+  } else {
+    // mongoose.connection.close()
+    fruits.forEach((fruit) => {
+      console.log(fruit.name);
+    })
+  }
+})
