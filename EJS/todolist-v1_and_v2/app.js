997ea6fd479a689bb0a2d048e3cc05f393c3b907
Moutasim02 <moutasim9997@gmail.com>
2023-04-08 08:03:02 +0400

    Skipped todo project v2

--- EJS/todolist-v1_and_v2/app.js
+++ EJS/todolist-v1_and_v2/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-
+// Stopped at 347
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -34,6 +34,15 @@ const task3 = new ToDoItem({
   task: "This is the third task!",
 });
 
+const defaultTasks = [task1, task2, task3];
+
+const listSchema = {
+  name: String,
+  items: [todoSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", (req, res) => {
   var today = new Date();
   var options = {
@@ -45,7 +54,6 @@ app.get("/", (req, res) => {
 
   ToDoItem.find({}, (err, foundItems) => {
     if (foundItems.length === 0) {
-      const defaultTasks = [task1, task2, task3];
       ToDoItem.insertMany(defaultTasks, (err) => {
         if (err) {
           console.log(err);
@@ -53,18 +61,47 @@ app.get("/", (req, res) => {
           console.log("Successfully added!");
         }
       });
-      res.redirect("/")
+      res.redirect("/");
     } else {
       res.render("list", { kindOfDay: day, newListItems: foundItems }); // The key (variable) is the same as the one in list.ejs
     }
-
   });
+});
+
+app.get("/:customListName", (req, res) => {
+  const customListName = req.params.customListName;
+
+  List.findOne({ name: customListName }, (err, foundList) => {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultTasks,
+        });
+        list.save();
+		res.redirect("/" + customListName);
+      } else {
+        res.render("list", {
+          kindOfDay: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
+});
+
+app.post("/delete", (req, res) => {
+  const taskToDelete = req.body.taskToDelete;
+  ToDoItem.findByIdAndRemove(taskToDelete, (err) => {
+    if (!err) console.log("Successfully Removed");
+  });
+  res.redirect("/");
 });
 
 app.post("/", (req, res) => {
   var taskName = req.body.todoInput;
-  const task = new ToDoItem ({task: taskName}) 
-  task.save()
+  const task = new ToDoItem({ task: taskName });
+  task.save();
   res.redirect("/");
 });
 
